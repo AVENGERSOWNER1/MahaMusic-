c7d6e51fde2ba5c52c1c806d773a5b8a471b7ab7
ISTKHAR-king <istkharalam225@gmail.com>
2025-09-08 16:45:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,10 +22,14 @@ PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
+
+LOGGER_ID = int(getenv("LOGGER_ID", None))
+
+# Get this value from @BRANDRD_ROBOT on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID", "7980630836"))
+
 LOG = int(getenv("LOG", True))
-LOGGER_ID = LOG_GROUP_ID
-# Get this value from @ISTKHAR_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "7633166578"))
+
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
